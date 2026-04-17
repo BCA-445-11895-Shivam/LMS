b4a192a2a77ee6fabf3dcfa14d65f5aecaf601e7
Shivam <shivam.bcastudent23.11895@cimage.in>
2026-04-18 04:06:26 +1400

    Add files via upload

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2026 Online Library Management System | Designed by : Shivam Raj</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
---
+++ library/adminlogin.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if($_SESSION['alogin']!=''){
+$_SESSION['alogin']='';
+}
+if(isset($_POST['login']))
+{
+
+$username=$_POST['username'];
+$password=md5($_POST['password']);
+$sql ="SELECT UserName,Password FROM admin WHERE UserName=:username and Password=:password";
+$query= $dbh -> prepare($sql);
+$query-> bindParam(':username', $username, PDO::PARAM_STR);
+$query-> bindParam(':password', $password, PDO::PARAM_STR);
+$query-> execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+if($query->rowCount() > 0)
+{
+$_SESSION['alogin']=$_POST['username'];
+echo "<script type='text/javascript'> document.location ='admin/dashboard.php'; </script>";
+} else{
+echo "<script>alert('Invalid Details');</script>";
+}
+}
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Online Library Management System</title>
+    <!-- BOOTSTRAP CORE STYLE  -->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+    <!-- FONT AWESOME STYLE  -->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+    <!-- CUSTOM STYLE  -->
+    <link href="assets/css/style.css" rel="stylesheet" />
+    <!-- GOOGLE FONT -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+    <style>
+        .about{
+            border:1px solid #FF6B6B;
+            display:flex;
+            flex-wrap:wrap;
+        }
+    </style>
+
+</head>
+<body>
+    <!------MENU SECTION START-->
+<?php include('includes/header.php');?>
+<!-- MENU SECTION END-->
+<div class="content-wrapper">
+<div class="container">
+<div class="row pad-botm">
+<div class="col-md-12">
+<h4 class="header-line">ADMIN LOGIN FORM</h4>
+</div>
+</div>
+             
+<!--LOGIN PANEL START-->           
+<div class="row">
+<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
+<div class="panel panel-info">
+<div class="panel-heading">
+ LOGIN FORM
+</div>
+<div class="panel-body">
+<form role="form" method="post">
+
+<div class="form-group">
+<label>Enter Username</label>
+<input class="form-control" type="text" name="username" autocomplete="off" required />
+</div>
+<div class="form-group">
+<label>Password</label>
+<input class="form-control" type="password" name="password" autocomplete="off" required />
+</div>
+
+ <button type="submit" name="login" class="btn btn-info">LOGIN </button>
+</form>
+ </div>
+</div>
+</div>
+</div>  
+<!---LOGIN PABNEL END-->            
+             
+ 
+    </div>
+    <div class="about">
+    <h1>   About Us My Library System</h1>
+    <ul>
+        <li><p>
+            Our Library Management System is designed to simplify and automate
+            the management of library operations. It helps administrators
+            efficiently manage books, members, issue/return records, and
+            overall library activities.
+        </p>
+        </li>
+        <li><p>
+            This system provides a secure and user-friendly interface for
+            administrators to maintain records, track book availability,
+            and generate reports, ensuring smooth and organized library
+            functioning.
+        </p></li>
+         <li> <p>
+            With advanced features and easy accessibility, our system
+            enhances productivity and ensures accurate data management
+            for modern libraries.
+        </p></li>
+        </ul>
+</div>
+    </div>
+     <!-- CONTENT-WRAPPER SECTION END-->
+ <?php include('includes/footer.php');?>
+      <!-- FOOTER SECTION END-->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!-- BOOTSTRAP SCRIPTS  -->
+    <script src="assets/js/bootstrap.js"></script>
+      <!-- CUSTOM SCRIPTS  -->
+    <script src="assets/js/custom.js"></script>
+</script>
+</body>
+</html>
